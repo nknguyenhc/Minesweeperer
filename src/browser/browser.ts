7375d564f4ec46070afefb83fd63775ef2580771
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-05-11 20:38:36 +0800

    Refactor browser for extension

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -31,47 +31,19 @@ const sleep = (ms: number) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-export class BrowserManager {
+export abstract class BrowserManager {
   private readonly width = 1440;
   private readonly height = 800;
-  private readonly url = 'https://google.com/search?q=minesweeper';
+  protected abstract readonly url: string;
   private readonly imageDirname = 'images';
   private browser: Browser | undefined = undefined;
   private page: Page | undefined = undefined;
 
-  private readonly gameMode: GameMode;
-  private readonly cellOffset = 1;
-  private readonly cellWidth;
-  private readonly xMax;
-  private readonly yMax;
-  private canvasPosition: Coordinate | undefined;
+  protected abstract readonly gameMode: GameMode;
+  protected abstract readonly xMax: number;
+  protected abstract readonly yMax: number;
 
   private count: number = 0;
-
-  constructor() {
-    switch (AppConfig.gameMode) {
-      case "easy":
-        this.gameMode = GameMode.EASY;
-        this.cellWidth = 45;
-        this.xMax = 10;
-        this.yMax = 8;
-        break;
-      case "medium":
-        this.gameMode = GameMode.MEDIUM;
-        this.cellWidth = 30;
-        this.xMax = 18;
-        this.yMax = 14;
-        break;
-      case "hard":
-        this.gameMode = GameMode.HARD;
-        this.cellWidth = 25;
-        this.xMax = 24;
-        this.yMax = 20;
-        break;
-      default:
-        throw new Error(`Unrecognised game mode: "${AppConfig.gameMode}"`);
-    }
-  }
 
   public async launchAndGo() {
     // Create directory if it does not exist
@@ -96,7 +68,7 @@ export class BrowserManager {
     return new Solver(this.xMax, this.yMax);
   }
 
-  private getPage(): Page {
+  protected getPage(): Page {
     return this.page as Page;
   }
 
@@ -104,53 +76,9 @@ export class BrowserManager {
     return this.browser as Browser;
   }
 
-  private getCanvasPosition(): Coordinate {
-    return this.canvasPosition as Coordinate;
-  }
+  public abstract startGame(): Promise<void>;
 
-  public async startGame() {
-    // Open the minefield
-    await (await this.getPage().$('.fxvhbc') as ElementHandle<Element>).click();
-    await sleep(100);
-
-    // Mute
-    await (await this.getPage().$('img.oO5WXb') as ElementHandle<Element>).click();
-
-    // Select difficulty
-    await (await this.getPage().$$('.CcNe6e'))[4].click();
-    await sleep(100);
-    await (await this.getPage().$(`.EpPYLd.GZnQqe.WtV5nd[data-difficulty=${GameMode[this.gameMode]}]`) as ElementHandle<Element>).click();
-    await sleep(100);
-
-    // Determine canvas position
-    this.canvasPosition = await this.getPage().evaluate(`
-      const rect = document.querySelector('canvas.ecwpfc').getBoundingClientRect();
-      result = { x: rect.x, y: rect.y };
-      result`) as Coordinate;
-  };
-
-  private async getNum(x: number, y: number): Promise<number> {
-    const imageData = await this.getPage().evaluate(
-      `document.querySelector('canvas.ecwpfc')
-        .getContext('2d')
-        .getImageData(${(x + 0.5) * this.cellWidth + 1}, ${(y + 0.5) * this.cellWidth}, ${this.cellOffset}, ${this.cellOffset})`) as ImageData;
-    const data = imageData.data;
-
-    let number: number = 0;
-    let smallestDistance: number = 3 * 255 ** 2;
-    for (let i = 0; i <= 8; i++) {
-      const newDistance = this.distance(data, numberRgb[i]);
-      if (newDistance < smallestDistance) {
-        smallestDistance = newDistance;
-        number = i;
-      }
-    }
-    return number;
-  };
-
-  private distance(rbg1: Uint8ClampedArray, rbg2: number[]): number {
-    return Math.sqrt((rbg1[0] - rbg2[0]) ** 2 + (rbg1[1] - rbg2[1]) ** 2 + (rbg1[2] - rbg2[2]) ** 2);
-  }
+  protected abstract getNum(x: number, y: number): Promise<number>;
 
   public async getNumbers(): Promise<number[][]> {
     this.count++;
@@ -171,13 +99,7 @@ export class BrowserManager {
     return numbers;
   }
 
-  public async openPosition(coordinate: Coordinate): Promise<void> {
-    await this.getPage().mouse.click(
-      this.getCanvasPosition().x + (coordinate.x + 0.5) * this.cellWidth,
-      this.getCanvasPosition().y + (coordinate.y + 0.5) * this.cellWidth);
-    await sleep(AppConfig.stepWaitTime);
-    return;
-  }
+  public abstract openPosition(coordinate: Coordinate): Promise<void>;
 
   public async openPositions(coordinates: Coordinate[]): Promise<void> {
     return Promise.all(coordinates.map(coordinate => this.openPosition(coordinate))).then(() => {});
@@ -196,5 +118,98 @@ export class BrowserManager {
 
   public async close() {
     await this.getBrowser().close();
+  }
+}
+
+export class GoogleBrowserManager extends BrowserManager {
+  protected override readonly url = 'https://google.com/search?q=minesweeper';
+
+  protected override readonly gameMode: GameMode;
+  protected override readonly xMax: number;
+  protected override readonly yMax: number;
+  private readonly cellOffset = 1;
+  private readonly cellWidth: number;
+  private canvasPosition: Coordinate | undefined;
+
+  constructor() {
+    super();
+    switch (AppConfig.gameMode) {
+      case "easy":
+        this.gameMode = GameMode.EASY;
+        this.cellWidth = 45;
+        this.xMax = 10;
+        this.yMax = 8;
+        break;
+      case "medium":
+        this.gameMode = GameMode.MEDIUM;
+        this.cellWidth = 30;
+        this.xMax = 18;
+        this.yMax = 14;
+        break;
+      case "hard":
+        this.gameMode = GameMode.HARD;
+        this.cellWidth = 25;
+        this.xMax = 24;
+        this.yMax = 20;
+        break;
+      default:
+        throw new Error(`Unrecognised game mode: "${AppConfig.gameMode}"`);
+    }
+  }
+
+  public override async startGame(): Promise<void> {
+    // Open the minefield
+    await (await this.getPage().$('.fxvhbc') as ElementHandle<Element>).click();
+    await sleep(100);
+
+    // Mute
+    await (await this.getPage().$('img.oO5WXb') as ElementHandle<Element>).click();
+
+    // Select difficulty
+    await (await this.getPage().$$('.CcNe6e'))[4].click();
+    await sleep(100);
+    await (await this.getPage().$(`.EpPYLd.GZnQqe.WtV5nd[data-difficulty=${GameMode[this.gameMode]}]`) as ElementHandle<Element>).click();
+    await sleep(100);
+
+    // Determine canvas position
+    this.canvasPosition = await this.getPage().evaluate(`
+      const rect = document.querySelector('canvas.ecwpfc').getBoundingClientRect();
+      result = { x: rect.x, y: rect.y };
+      result`) as Coordinate;
+  }
+
+  private getCanvasPosition(): Coordinate {
+    return this.canvasPosition as Coordinate;
+  }
+
+  protected override async getNum(x: number, y: number): Promise<number> {
+    const imageData = await this.getPage().evaluate(
+      `document.querySelector('canvas.ecwpfc')
+        .getContext('2d')
+        .getImageData(${(x + 0.5) * this.cellWidth + 1}, ${(y + 0.5) * this.cellWidth}, ${this.cellOffset}, ${this.cellOffset})`) as ImageData;
+    const data = imageData.data;
+
+    let number: number = 0;
+    let smallestDistance: number = 3 * 255 ** 2;
+    for (let i = 0; i <= 8; i++) {
+      const newDistance = this.distance(data, numberRgb[i]);
+      if (newDistance < smallestDistance) {
+        smallestDistance = newDistance;
+        number = i;
+      }
+    }
+    return number;
+  }
+
+  private distance(rbg1: Uint8ClampedArray, rbg2: number[]): number {
+    return Math.sqrt((rbg1[0] - rbg2[0]) ** 2 + (rbg1[1] - rbg2[1]) ** 2 + (rbg1[2] - rbg2[2]) ** 2);
+  }
+
+  public override async openPosition(coordinate: Coordinate): Promise<void> {
+    await this.getPage().mouse.click(
+      this.getCanvasPosition().x + (coordinate.x + 0.5) * this.cellWidth,
+      this.getCanvasPosition().y + (coordinate.y + 0.5) * this.cellWidth);
+    await sleep(AppConfig.stepWaitTime);
+    return;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { AppConfig } from "./appconfig";
-import { BrowserManager } from "./browser/browser";
+import { BrowserManager, GoogleBrowserManager } from "./browser/browser";
 
 async function main() {
-  const browserManager = new BrowserManager();
+  const browserManager: BrowserManager = new GoogleBrowserManager();
   await browserManager.launchAndGo();
   await browserManager.startGame();
   await browserManager.openInitial();
